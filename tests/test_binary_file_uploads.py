0d4c1be3cb747d9c0f52dd9202800689183be830
Ben Altieri <chiefspace@gmail.com>
2017-03-09 14:04:50 +0000

    added comments to test_binary_file_uploads.py

--- tests/test_binary_file_uploads.py
+++ tests/test_binary_file_uploads.py
@@ -3,20 +3,24 @@ from blog import app
 from io import BytesIO
 
 class UploadTest(unittest.TestCase):
-   def setUp(self):
+    """ SetUp """
+    def setUp(self):
         self.app = app
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
 
-   def test_upload(self):
+    """ Test allowed file extension binary file upload """
+    def test_upload(self):
         res1 = self.client.post('/upload', data=dict(file=(BytesIO(b"this is a test"), 'test.jpg'),), follow_redirects=True)
         assert res1.status_code == 200
         assert b'file saved' in res1.data
-      
-   def test_disallowed_file_type(self):
+
+    """ Test disallowed file extension binary file upload """      
+    def test_disallowed_file_type(self):
         res3 = self.client.post('/upload', data=dict(file=(BytesIO(b"this is a test"), 'test.txt'),), follow_redirects=True)
         assert b'file extension not allowed' in res3.data
 
+    """ Tear Down """
 
 if __name__ == '__main__':
    unittest.main()
