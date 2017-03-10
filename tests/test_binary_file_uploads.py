334d0e558e288cfee305a09ec6fba6e339b38316
Ben Altieri <chiefspace@gmail.com>
2017-03-10 04:09:09 +0000

    added setUp and teardown code to test_binary_file_uploads.py

--- tests/test_binary_file_uploads.py
+++ tests/test_binary_file_uploads.py
@@ -1,10 +1,31 @@
 import unittest, os
+import multiprocessing
+import time
 from blog import app
 from io import BytesIO
+from urllib.parse import urlparse
+
+from werkzeug.security import generate_password_hash
+
+from blog.database import Base, engine, session, User
 
 class UploadTest(unittest.TestCase):
     """ SetUp """
     def setUp(self):
+        # Set up the tables in the database
+        Base.metadata.create_all(engine)
+
+        # Create an example user
+        self.user = User(name="Alice", email="alice@example.com",
+                         password=generate_password_hash("test"))
+        session.add(self.user)
+        session.commit()
+
+        self.process = multiprocessing.Process(target=app.run,
+                                               kwargs={"port": 8080})
+        self.process.start()
+        time.sleep(2)
+
         self.app = app
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
@@ -21,6 +42,13 @@ class UploadTest(unittest.TestCase):
         assert b'file extension not allowed' in res3.data
 
     """ Tear Down """
+    def tearDown(self):
+        """ Test teardown """
+        # Remove the tables and their data from the database
+        self.process.terminate()
+        session.close()
+        engine.dispose()
+        Base.metadata.drop_all(engine)
 
 if __name__ == '__main__':
    unittest.main()
